fa2b0b2a5eba3011709258852d09f55d532eaf77
matheusouza30 <131723419+matheusouza30@users.noreply.github.com>
2023-07-07 22:11:48 -0300

    Atividade Final PDM - 07/07/23
    Matheus Souza, 3ºB 07/07/23

--- main.dart
+++ main.dart
@@ -1,48 +1,97 @@
 import 'package:flutter/material.dart';
-import 'questionario.dart';
+import 'dart:math';
 
 void main() {
-  runApp(AulaComponentes());
-}
-
-class AulaComponentes extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Inicio(),
-    );
-  }
-}
-
-class Inicio extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  runApp(MaterialApp(
+    home: Scaffold(
       appBar: AppBar(
-        title: Text('Jogo de perguntas e respostas'),
+        title: Text('Ficha Estudantil'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Ficha(
+                foto: 'https://media.istockphoto.com/id/1278139568/pt/foto/studio-portrait-of-20-year-old-woman.jpg?s=612x612&w=0&k=20&c=NipIoQzg7TsYHdYdOsLkncvLKof7Xp8MQTuzMWD0ZOI=',
+                nome: 'Luisa Gontijo',
+                matricula: generateRandomNumber(),
+                escola: 'Cefet - Campus V',
+                serie: '1º ano',
+              ),
+              SizedBox(height: 20),
+              Ficha(
+                foto: 'https://storage.alboom.ninja/sites/56081/albuns/1096019/221028_iuryesquarcio_8528.jpg?t=1669214646',
+                nome: 'Gabriel Henrique Castro Alves',
+                matricula: generateRandomNumber(),
+                escola: 'Escola Estadual Joaquim Nabuco',
+                serie: '3º ano',
+              ),
+              SizedBox(height: 20),
+              Ficha(
+                foto: 'https://media.licdn.com/dms/image/C4E03AQEZlbfytKCycQ/profile-displayphoto-shrink_800_800/0/1555370121979?e=2147483647&v=beta&t=tWxc7lSVTKRGiumJJ7XIJpchZvQy9WxVEmWrUU8Py0A',
+                nome: 'Marcela Silva de Andrade',
+                matricula: generateRandomNumber(),
+                escola: 'Universidade Federal de Minas Gerais',
+                serie: '4º Período',
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ));
+}
+
+class Ficha extends StatelessWidget {
+  final String foto;
+  final String nome;
+  final int matricula;
+  final String escola;
+  final String serie;
+
+  const Ficha({
+    required this.foto,
+    required this.nome,
+    required this.matricula,
+    required this.escola,
+    required this.serie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              height: 150,
+              width: 150,
+              child: Image.network(
+                foto,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
             Text(
-              'Aqui haverá um questionário de perguntas e respostas, divirta-se!',
-              style: TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
+              'Nome: $nome',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Questionario()),
-                );
-              },
-              child: Text('Iniciar'),
-            ),
+            SizedBox(height: 5),
+            Text('Matrícula: $matricula'),
+            SizedBox(height: 5),
+            Text('Escola: $escola'),
+            SizedBox(height: 5),
+            Text('Série: $serie'),
           ],
         ),
       ),
     );
   }
+}
+
+int generateRandomNumber() {
+  Random random = Random();
+  return random.nextInt(100000);
 }
